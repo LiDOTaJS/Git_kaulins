abcff984073b89aecf4b59a222a915ce5412c878
AliasXIX <arnis.senbruns@sk.lvt.lv>
2025-04-07 12:08:18 +0300

    Veikti koda uzlabojumi

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,25 +3,40 @@ import java.util.Scanner;
 
 public class Kaulins {
 
-	static void mestKaulinu(int reizes) {
+	static int mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
 		for(int i=0; i<reizes; i++) { 
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
+		return skaitlis;
 	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int reizes;
+		int reizes, izvele, pedejais;
 		do {
-			System.out.println("Cik reizes mest kauliņu?");
-			reizes = scan.nextInt();
-		}while (reizes <1);
-		scan.close();
-		mestKaulinu(reizes);
-	
-		
+			System.out.println("1 - Mest kauliņu | 2 - Apskatīt pēdējo | 3 - Apturēt");
+			izvele = scan.nextInt();
+			switch(izvele){
+				case 1:
+					do{
+						System.out.println("Cik reizes mest kauliņu");
+						reizes = scan.nextInt();
+					}while(reizes<1);
+					pedejais = mestKaulinu(reizes);
+					break;
+				case 2:
+					System.out.println("Pēdējais uzmestais ir " + pedejais);
+					break;
+				case 3:
+					System.out.println("Programma apturēta!");
+					break;
+				default:
+					System.out.println("Nav tāda darbība!");
+			}
+		}while (izvele != 3);
+		scan.close();	
 	}
 }
